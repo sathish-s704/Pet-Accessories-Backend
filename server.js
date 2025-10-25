f5f2bec12c3851e3f48e42504403e72ef0fbf0c0
sathish-s704 <sathishsathish75363@gmail.com>
2025-10-25 20:00:12 +0530

    server file changed

--- server.js
+++ server.js
@@ -72,5 +72,8 @@ app.use((err, req, res, next) => {
   console.error('❌ Express Error:', err.message);
   res.status(500).json({ message: err.message });
 });
-
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`✅ Server running on port ${PORT}`);
+});
 export default app;
